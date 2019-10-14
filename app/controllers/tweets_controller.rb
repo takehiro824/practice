ef02b01b3303df818549f6d00b8695e6aa784437
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-10-14 21:15:05 +0900

    tweet_function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
   def index 
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user_id == current_user.id
+      @tweet.destroy
+      redirect_to root_path
+    end
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_135026) do
+ActiveRecord::Schema.define(version: 2019_10_14_115140) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.string "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2019_09_26_135026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
